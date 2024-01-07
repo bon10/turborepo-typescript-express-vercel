d7b91731e0b8cba5ba52b28a5e3a29686e787c92
bone <bontyun@gmail.com>
2024-01-07 22:05:09 +0900

    fix

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -11,12 +11,18 @@ export const createServer = (): Express => {
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
+    .get("/api", (req, res) => {
+      return res.send("Express on Vercel");
+    })
     .get("/message/:name", (req, res) => {
       return res.json({ message: `hello ${req.params.name}` });
     })
     .get("/status", (_, res) => {
       return res.json({ ok: true });
-    });
+    })
+    .get("/", (req, res) => {
+      return res.json({ message: 'OK' });
+    })
 
   return app;
 };
